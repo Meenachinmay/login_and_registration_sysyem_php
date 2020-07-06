a52acea280c186943819f946a8eaa616762a6cb7
meenachinmay <chinmayanand896@gmail.com>
2020-07-06 00:37:08 -0700

    bootstrap style added to login and registration form

--- login.php
+++ login.php
@@ -3,35 +3,38 @@
 <html>
 <head>
 	<title>Registration system PHP and MySQL</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
 	integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
+	<div class="container p-5">
+		<div class="card">
+			<div class="card-header">
+				Login Form
+			</div>
+			<div class="card-body">
+				<form method="post" action="login.php">
 
-	<div class="header">
-		<h2>Login</h2>
+					<?php include('errors.php'); ?>
+
+					<div class="form-group">
+						<label>Username</label>
+						<input type="email" name="username" class="form-control" placeholder="Enter email...">
+					</div>
+					<div class="form-group">
+						<label>Password</label>
+						<input type="password" name="password" class="form-control" placeholder="Enter password...">
+					</div>
+					<div class="input-group">
+						<button type="submit" class="btn btn-primary" name="login_user">Login</button>
+					</div>
+					<p>
+						Not yet a member? <a href="register.php">Sign up</a>
+					</p>
+				</form>
+			</div>
+		</div>
 	</div>
-	
-	<form method="post" action="login.php">
-
-		<?php include('errors.php'); ?>
-
-		<div class="input-group">
-			<label>Username</label>
-			<input type="text" name="username" >
-		</div>
-		<div class="input-group">
-			<label>Password</label>
-			<input type="password" name="password">
-		</div>
-		<div class="input-group">
-			<button type="submit" class="btn" name="login_user">Login</button>
-		</div>
-		<p>
-			Not yet a member? <a href="register.php">Sign up</a>
-		</p>
-	</form>
 
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" 
 	integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
--- register.php
+++ register.php
@@ -3,42 +3,49 @@
 <html>
 <head>
 	<title>Registration system PHP and MySQL</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
 	integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-	<div class="header">
-		<h2>Register</h2>
+	<div class="container p-5">
+		<div class="card">
+			<div class="card-header">
+				Registration Form
+			</div>
+			<div class="card-body">
+				<form method="post" action="register.php">
+
+					<?php include('errors.php'); ?>
+
+					<div class="form-group">
+						<label>Username</label>
+						<input type="text" name="username" class="form-control" 
+						value="<?php echo $username; ?>" placeholder="Enter username...">
+					</div>
+					<div class="form-group">
+						<label>Email</label>
+						<input type="text" name="email" class="form-control" placeholder="Enter email..." 
+						value="<?php echo $email; ?>">
+					</div>
+					<div class="form-group">
+						<label>Password</label>
+						<input type="password" class="form-control" placeholder="Enter password..." name="password_1">
+					</div>
+					<div class="form-group">
+						<label>Confirm password</label>
+						<input type="password" name="password_2" class="form-control" placeholder="Enter confirm password...">
+					</div>
+					<div class="input-group">
+						<button type="submit" class="btn btn-primary" name="reg_user">Register</button>
+					</div>
+					<p>
+						Already a member? <a href="login.php">Sign in</a>
+					</p>
+				</form>
+			</div>
+		</div>
 	</div>
-	
-	<form method="post" action="register.php">
 
-		<?php include('errors.php'); ?>
-
-		<div class="input-group">
-			<label>Username</label>
-			<input type="text" name="username" value="<?php echo $username; ?>">
-		</div>
-		<div class="input-group">
-			<label>Email</label>
-			<input type="text" name="email" value="<?php echo $email; ?>">
-		</div>
-		<div class="input-group">
-			<label>Password</label>
-			<input type="password" name="password_1">
-		</div>
-		<div class="input-group">
-			<label>Confirm password</label>
-			<input type="password" name="password_2">
-		</div>
-		<div class="input-group">
-			<button type="submit" class="btn" name="reg_user">Register</button>
-		</div>
-		<p>
-			Already a member? <a href="login.php">Sign in</a>
-		</p>
-	</form>
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" 
 	integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" 
